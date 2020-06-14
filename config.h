c51f6a5fa4df59ae7922adc02d1b6ff4993f9489
sam brysbaert <sam.brysbaert@gmail.com>
2020-06-14 11:54:00 +0200

    remove unnecessary dmenu font line

--- config.h
+++ config.h
@@ -6,7 +6,6 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Roboto Mono:size=11" };
-static const char dmenufont[]       = "Roboto Mono:size=11";
 static const char bg_normal[]       = "#282a36";
 static const char bg_selected[]     = "#5f4a7d";
 static const char border_normal[]   = "#000000";
