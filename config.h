2d4152802c05a542c0ede7a9d6b56dd2f448a1ca
sam brysbaert <sam.brysbaert@gmail.com>
2020-06-14 11:30:21 +0200

    change colors to dracula theme

--- config.h
+++ config.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Roboto Mono:size=11" };
+static const char dmenufont[]       = "Roboto Mono:size=11";
+static const char bg_normal[]       = "#282a36";
+static const char bg_selected[]     = "#5f4a7d";
+static const char border_normal[]   = "#000000";
+static const char fg[]              = "#f8f8f2";
+static const char border_selected[] = "#eaeaea";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { fg, bg_normal, border_normal },
+	[SchemeSel]  = { fg, bg_selected,  border_selected },
 };
 
 /* tagging */
